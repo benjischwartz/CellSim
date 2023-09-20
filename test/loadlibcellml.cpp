e5ec725f6686c4abf9503e894560f27afe09dd22
Benji Schwartz <benji.schwartz2013@gmail.com>
2023-09-20 16:19:38 +1000

    second attempt

---
+++ test/loadlibcellml.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include <libcellml>
+
+int main() {
+    std::cout << "LibCellML version: " << LIBCELLML_VERSION_MAJOR
+              << "." << LIBCELLML_VERSION_MINOR << std::endl;
+    return 0;
+}
+
