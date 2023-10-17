8fc51d459efb38a52572219766a9371f09b42d95
Benji Schwartz <benji.schwartz2013@gmail.com>
2023-10-17 14:02:46 +1100

    Added file upload option

--- frontend/CellSim_frontend/src/components/Visualise.vue
+++ frontend/CellSim_frontend/src/components/Visualise.vue
@@ -1,9 +1,12 @@
 <template> 
   <div>
     <h1>Visualise CellML File</h1>
+    <!-- Add input for file upload -->
+	<input type="file" @change="handleFileUpload" accept=".xml" />
+    <br /><br />
     <!-- Add text boxes for model name and model id -->
-    <textarea name="file" v-model="file" placeholder="Paste File Data Here"></textarea> <br /><br />
-    <button v-on:click="sendData()">Visualise</button> <br /><br />
+    <textarea name="file" v-model="file" placeholder="Or paste File Data Here"></textarea> <br /><br />
+    <button @click="sendData()">Visualise</button> <br /><br />
   </div>
 
     <!-- Display the result data in a panel -->
@@ -30,6 +33,16 @@ export default {
   },
 
   methods:{
+	handleFileUpload(event) {
+      const selectedFile = event.target.files[0];
+      if (selectedFile) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          this.file = e.target.result;
+        };
+        reader.readAsText(selectedFile);
+      }	
+	},
     async sendData()
     {
       let result = await axios.post("http://localhost:8000/api/visualise", {
