2a7e8cff2c3ac3753e303cbae92a4d3587ca0ef4
Benji Schwartz <benji.schwartz2013@gmail.com>
2023-10-20 11:23:09 +1100

    Starting from stripped back example. Converting first to JS object which will then be processed

--- frontend/CellSim_frontend/src/components/Visualise.vue
+++ frontend/CellSim_frontend/src/components/Visualise.vue
@@ -27,12 +27,11 @@ import * as d3 from 'd3';
 import xml2js from 'xml2js';
 
 export default {
-  name: "Visualise",
+  name: "visualise",
   data() {
     return {
       file: "",
       resultData: null,
-      xmlData: "",
     };
   },
   methods: {
@@ -51,97 +50,27 @@ export default {
         file: this.file
       });
       this.resultData = result;
-      this.xmlData = this.file;
       console.warn("function called", this.file);
 
       this.visualizeData();
     },
-	transformData(xmlObject) {
-	  const root = {
-	    name: "root",
-	    children: [],
-	  };
-	
-	  function transformNode(node) {
-	    const newNode = {
-	      name: node.$.name || "Unnamed Node",
-	      children: [],
-	    };
-	
-	    if (node.variable) {
-	      newNode.children = node.variable.map(transformNode);
-	    } else if (node.component) {
-	      newNode.children = node.component.map(transformNode);
-	    }
-	
-	    return newNode;
-	  }
-	
-	  if (Array.isArray(xmlObject.model)) {
-	    root.children = xmlObject.model.map(transformNode);
-	  } else if (xmlObject.model) {
-        root.children.push(transformNode(xmlObject.model));
+    visualizeData() {
+      if (!this.file) {
+        console.error("No XML data to visualize.");
+        return;
       }
-	
-	  return root;
-	},
-	
-	visualizeData() {
-	  if (!this.xmlData) {
-	    console.error("No XML data to visualize.");
-	    return;
-	  }
-	
-	  xml2js.parseString(this.xmlData, (err, result) => {
-	    if (!err) {
-	      const treeData = this.transformData(result);
-
-          // calculate tree dimensions
-      	  const treeLayout = d3.tree().size([1200, 800]); // Adjust these values as needed
-      	  const treeRoot = d3.hierarchy(treeData);
-      	  const treeLayoutData = treeLayout(treeRoot);
-
-		  // Calculate the required SVG container dimensions
-      	  const treeWidth = treeLayoutData.descendants().length * 200; // Adjust as needed
-      	  const treeHeight = treeLayoutData.height * 200; // Adjust as needed
-	
-	      d3.select("#xml-visualization-container").html("");
-	
-	      const svg = d3
-	        .select("#xml-visualization-container")
-	        .append("svg")
-	        .attr("width", treeWidth)
-	        .attr("height", treeHeight);
-	
-	      const links = svg
-	        .selectAll(".link")
-	        .data(treeLayoutData.links())
-	        .enter()
-	        .append("path")
-	        .attr("class", "link")
-	        .attr("d", (d) => {
-                return `M${d.source.x},${d.source.y} L${d.target.x},${d.target.y}`
-            });
-	
-	      const nodes = svg
-	        .selectAll(".node")
-	        .data(treeLayoutData.descendants())
-	        .enter()
-	        .append("g")
-	        .attr("class", "node")
-	        .attr("transform", (d) => `translate(${d.x},${d.y})`);
-	
-	      nodes.append("circle").attr("r", 10);
-	
-	      nodes
-	        .append("text")
-	        .attr("dy", ".35em")
-	        .attr("y", (d) => (d.children ? -20 : 20))
-	        .style("text-anchor", "middle")
-	        .text((d) => d.data.name);
-	    }
-	  });
-	},
+      // xml data stored in this.file
+      // use xml2json to transform to javascript object
+      console.log("original data -> %s", this.file);
+      xml2js.parseString(this.file, (err, result) => {
+        if (err) {
+          throw err;
+        }
+          const jsData = result;
+          const str = JSON.stringify(jsData, null, 2);
+          console.log("js -> %s", str);
+      });
+    },
   },
 };
 </script>
