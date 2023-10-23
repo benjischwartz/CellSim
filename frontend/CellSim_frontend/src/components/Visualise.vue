356f43d596dbddc194bde638e119973bb74cf73f
Benji Schwartz <benji.schwartz2013@gmail.com>
2023-10-23 16:44:27 +1100

    making progress... printing rectangles for all components and variables

--- frontend/CellSim_frontend/src/components/Visualise.vue
+++ frontend/CellSim_frontend/src/components/Visualise.vue
@@ -17,9 +17,13 @@
   </div>
   <br /><br />
 
-    
-  <!-- Add an SVG container for visualization -->
-  <div id="svg-container"></div>
+  <div class="rectangle" v-for="(text, index) in components" :key="index">
+    {{ text }}
+  </div>
+
+  <div class="rectangle" v-for="(text, index) in variables" :key="index">
+    {{ text }}
+  </div>
 
 </template>
 
@@ -34,7 +38,9 @@ export default {
     return {
       file: "",
       resultData: null,
-      treeData: "",
+      jsData: "",
+      components: [],
+      variables: [],
     };
   },
   methods: {
@@ -58,103 +64,42 @@ export default {
         if (err) {
           throw err;
         }
-        const jsData = result;
-        const str = JSON.stringify(jsData, null, 2);
+        this.jsData = result;
+        const str = JSON.stringify(result, null, 2);
         console.log("js -> %s", str);
-        this.treeData = jsData;
       });
       this.visualizeData();
     },
 
     visualizeData() {
-      const dx = 12;
-      const dy = 120;
-      var tree = d3.tree().nodeSize([dx, dy]);
-      var treeLink = d3.linkHorizontal().x(d => d.y).y(d => d.x);
-      function graph(root, {
-        label = d => d.data.id, 
-        highlight = () => false,
-        marginLeft = 40
-      } = {}) {
-        root = tree(root);
+      // creates list of components
+      this.jsData.model.component.forEach((component) => {
+        this.components.push(component.$.name);
+        if (component.variable) {
+          // if component has variables, add them to list
+          console.log("has variables");
+          component.variable.forEach((variable) => {
+            this.variables.push(variable.$.name);
+          })
+        }    
+      })
 
-        let x0 = Infinity;
-        let x1 = -x0;
-        root.each(d => {
-          if (d.x > x1) x1 = d.x;
-          if (d.x < x0) x0 = d.x;
-        });
-
-        const treeWidth = x1 - x0 + dx * 2;
-        const treeHeight = root.height * dy;
-
-        // Adjust the viewBox and container size based on the tree dimensions
-        const width = treeWidth + marginLeft;
-        const height = treeHeight + dx;
-
-        const svg = d3.create("svg")
-            .attr("viewBox", [0, 0, width, height])
-            .style("overflow", "visible");
-        
-        const g = svg.append("g")
-            .attr("font-family", "sans-serif")
-            .attr("font-size", 10)
-            .attr("transform", `translate(${marginLeft},${dx})`);
-          
-        const link = g.append("g")
-          .attr("fill", "none")
-          .attr("stroke", "#555")
-          .attr("stroke-opacity", 0.4)
-          .attr("stroke-width", 1.5)
-        .selectAll("path")
-          .data(root.links())
-          .join("path")
-            .attr("stroke", d => highlight(d.source) && highlight(d.target) ? "red" : null)
-            .attr("stroke-opacity", d => highlight(d.source) && highlight(d.target) ? 1 : null)
-            .attr("d", treeLink);
-        
-        const node = g.append("g")
-            .attr("stroke-linejoin", "round")
-            .attr("stroke-width", 3)
-          .selectAll("g")
-          .data(root.descendants())
-          .join("g")
-            .attr("transform", d => `translate(${d.y},${d.x})`);
-
-        node.append("circle")
-            .attr("fill", d => highlight(d) ? "red" : d.children ? "#555" : "#999")
-            .attr("r", 2.5);
-
-        node.append("text")
-            .attr("fill", d => highlight(d) ? "red" : null)
-            .attr("stroke", "white")
-            .attr("paint-order", "stroke")
-            .attr("dy", "0.31em")
-            .attr("x", d => d.children ? -6 : 6)
-            .attr("text-anchor", d => d.children ? "end" : "start")
-            .text(label);
-        
-        // Append the generated SVG to the #svg-container
-        d3.select("#svg-container").node().appendChild(svg.node());
-      }
-    var family = d3.hierarchy({
-      name: "root",
-      children: [
-        {name: "child #1"},
-        {
-          name: "child #2",
-          children: [
-            {name: "grandchild #1"},
-            {name: "grandchild #2"},
-            {name: "grandchild #3"}
-          ]
-        }
-      ]
-    })
-
-      graph(family, { label: d => d.data.name });
+      // creates list of variables for each component
+      
     },
   },
 }
 
 </script>
+
+<style>
+.rectangle {
+  width: 200px;
+  height: 100px;
+  background-color: lightblue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+}
+</style>
